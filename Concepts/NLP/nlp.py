aee1dbe7e7737f0550a9588d8355432899b5e568
Jesse van der Hulle <jessevanderhulle@gmail.com>
2023-04-18 21:50:31 +0200

    update nlp

--- Concepts/NLP/nlp.py
+++ Concepts/NLP/nlp.py
@@ -11,7 +11,7 @@ print("""
 from keytotext import pipeline
 
 nlp = pipeline("mrm8488/t5-base-finetuned-common_gen")  #loading the pre-trained model
-params = {"do_sample":True, "num_beams":4, "no_repeat_ngram_size":3, "early_stopping":True, "max_new_tokens":100}    #decoding params
+#params = {"do_sample":True, "num_beams":1, "no_repeat_ngram_size":3, "early_stopping":True, "max_new_tokens":100}    #decoding params
 
 while True:
     keywords = []
@@ -24,4 +24,4 @@ while True:
         else:
             break
 
-    print (nlp(keywords, **params))  #keywords
+    print (nlp(keywords))#, **params))  #keywords

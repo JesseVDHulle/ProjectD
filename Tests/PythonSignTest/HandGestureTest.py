abd468878f44b82880809c36169bc5003776ffe4
Avexces <46761876+Avexces@users.noreply.github.com>
2023-05-17 10:59:12 +0200

    daadwerkelijke bestanden

--- Tests/PythonSignTest/HandGestureTest.py
+++ Tests/PythonSignTest/HandGestureTest.py
@@ -17,15 +17,31 @@ while True:
     fps = 1/(cTime-pTime)
     pTime = cTime
 
-    hands, img = detector.findHands(img, flipType=False)
+    hands, img = detector.findHands(img, flipType=True)
     text = "none"
     text2 = "none"
+    orientation = "none"
+
     if hands:
         hand = hands[0]
+        type = hand['type']
+        lmlist = hand['lmList']
+        xPinky = lmlist[17][0]
+        xThumb = lmlist[2][0]
         fingers = detector.fingersUp(hand)
         print(fingers)
-        text2 = fingers
-        if fingers == [0,1,0,0,1]:
+        text2 = str(fingers)
+        if xPinky > xThumb:
+            if type == 'Right':
+                orientation ='BACK'
+            else:
+                orientation = 'PALM'
+        else:
+            if type == 'Right':
+                orientation = 'PALM'
+            else:
+                orientation = 'BACK'
+        if fingers == [1,1,0,0,1]  and orientation == 'PALM' :
             text = 'I LOVE YOU'
         if fingers == [0, 0, 1, 0, 0] or fingers == [1,0,1,0,0]:
             text ='FUCK TENSOR FLOW'
@@ -34,13 +50,13 @@ while True:
     x, y, w, h = 0, 0, image_width, 50
     cv2.rectangle(img, (x, x), (x + w, y + h), (50, 50, 50), -1)
     coordinates = (50, 35)
-    coordinates2 = (750, 35)
+    coordinates2 = (800, 35)
     font = cv2.FONT_HERSHEY_SIMPLEX
-    fontScale = 1
+    fontScale = .9
     color = (255, 155, 155)
     thickness = 2
-    img = cv2.putText(img, 'ASL: ' + text, coordinates, font, fontScale, color, thickness, cv2.LINE_AA)
-    img = cv2.putText(img, 'FINGERS: ' + text2, coordinates2, font, fontScale, color, thickness, cv2.LINE_AA)
+    img = cv2.putText(img, 'ASL: ' + text +' | ORIENTATION: '+ orientation, coordinates, font, fontScale, color, thickness, cv2.LINE_AA)
+    img = cv2.putText(img, ' | FINGERS: ' + text2, coordinates2, font, fontScale, color, thickness, cv2.LINE_AA)
     #            3, (255,0,255),3)
     cv2.imshow("Img", img)
     k = cv2.waitKey(30) & 0xff
